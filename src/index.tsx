2451f7487a6f1f3f85efba4c6f73cde8ccfcd29e
Patrício dos Santos <hello@psantos.dev>
2020-07-27 12:05:02 +0100

    Support passing className

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React, { Component, ElementType } from "react";
 import metismenujs from "metismenujs";
 import { IMMOptions } from "metismenujs/dist/types/interface";
 
-class MetisMenu extends Component<IMMOptions, {}> {
+interface IMetisMenuOptions extends IMMOptions {
+  className?: string
+}
+
+class MetisMenu extends Component<IMetisMenuOptions, {}> {
   protected mm!: metismenujs;
   protected el!: string | Element;
 
@@ -15,10 +19,10 @@ class MetisMenu extends Component<IMMOptions, {}> {
   }
 
   render() {
-    const { subMenu, children } = this.props;
+    const { subMenu, children, className } = this.props;
     const MMTag = subMenu || ("ul" as ElementType);
     return (
-      <MMTag ref={(el: string | Element) => (this.el = el)}>{children}</MMTag>
+      <MMTag className={`metismenu ${className}`} ref={(el: string | Element) => (this.el = el)}>{children}</MMTag>
     );
   }
 }
